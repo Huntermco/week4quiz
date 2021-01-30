b4fb00adb31d7189fe23c6219ccbc0a8ecb2042a
huntermco <hunterbeans27@gmail.com>
2021-01-29 22:53:33 -0700

    week4hw

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,23 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Which character from the Office dressed up as the Joker for Halloween?",
+    choices: ["Kevin", "Dwight", "Creed", "All of the Above"],
+    answer: "All of the Above"
+  },
+  {
+    title: "How did Robert Dunder and Robert Mifflin meet?",
+    choices: ["On Easy Street", "A tour of Dartmouth College", "they never ment", "brushing their teeth"],
+    answer: "A tour of Dartmouth College"
+  },
+  {
+    title: "Does it ever feel too hard to keep going?",
+    choices: ["Yes", "No", "That's what she said"],
+    answer: "That's what she said"
+  },
+  {
+    title: "What kind of farm does Dwight own?",
+    choices: ["Beet Farm", "Rock Farm", "Peanut and Grape hybrid"],
+    answer: "Beet Farm"
+  }
+];
